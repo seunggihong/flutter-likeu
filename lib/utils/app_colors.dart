cf28c003aecfbd96103bd938b7a00a365b4c5648
seunggi <hsk1533@naver.com>
2024-10-04 21:08:09 +0900

    fix: heatmap view

--- lib/utils/app_colors.dart
+++ lib/utils/app_colors.dart
@@ -3,4 +3,9 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const Color primaryColor = Color(0xffEE9849);
   static const Color backGroundColor = Colors.black;
+  static const Map<int, Color> heatMapColor = {
+    1: AppColors.primaryColor,
+    3: AppColors.primaryColor,
+    5: AppColors.primaryColor,
+  };
 }
