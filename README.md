f476233981d9bfeba8396b21fcb9ae7a27f635a0
snilsnil <heo6601@naver.com>
2024-10-17 23:05:29 +0900

    test by snilsnil

--- README.md
+++ README.md
@@ -1,17 +1,7 @@
 # flutter_likeu
 
-A new Flutter project.
+- /lib/views/camera/camera_view.dart에서 gallary를 누르면 갤러리에서 동영상을 선택하고 upload를 누르면 flask로 http post해서 동영상 전송됨.
 
-## Getting Started
+- 전송된 동영상을 flask에서 yolov8을 통해 keypoint와 관걸 각도를 추출하여 csv파일로 저장후 camera_view.dart로 csv파일을 return함.
 
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
-# flutter-likeu
+- return된 csv파일을 열어서 내용을 출력함.
--- lib/views/camera/camera_view.dart
+++ lib/views/camera/camera_view.dart
@@ -1,15 +1,13 @@
 import 'dart:developer';
 import 'dart:io';
-
+import 'package:csv/csv.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_likeu/extensions/space_exs.dart';
-// import 'package:flutter_likeu/utils/app_colors.dart';
 import 'package:flutter_likeu/views/camera/components/custom_button.dart';
-import 'package:flutter_likeu/views/camera/components/result_viewer.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
-// import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
-// import 'package:image_picker/image_picker.dart';
+import 'package:http/http.dart' as http;
+import 'package:path_provider/path_provider.dart';
 
 class CameraView extends StatefulWidget {
   const CameraView({super.key});
@@ -19,76 +17,71 @@ class CameraView extends StatefulWidget {
 }
 
 class _CameraViewState extends State<CameraView> {
-  File? _image;
-  final picker = ImagePicker();
-
-  get getImage => null;
-
-  // List<String> playerName = [
-  //   'Stephen Curry',
-  //   'Michael Jordan',
-  //   'LeBron James',
-  //   'Shaquille ONeal',
-  //   'Magic Johnson'
-  // ];
-
-  // String dropdownItem = 'Stephen Curry';
+  XFile? _video;
+  final ImagePicker _picker = ImagePicker();
+  List<List<dynamic>> _csvData = []; // CSV 데이터를 저장할 리스트
+  bool _isLoading = false; // 로딩상태 추적
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            60.h,
-
-            /// Image Loader
-            _loadPickerImageBox(),
-
-            /// Open Gallery Or Camera Button Widget.
-            _loadGalleryOrCameraButton()
-          ],
-        ));
-  }
-
-  /// Image Picker Loader
-  Widget _loadPickerImageBox() {
-    return SizedBox(
-      height: 400,
-      child: Center(
-        child: Container(
-          width: double.infinity,
-          height: 500,
-          alignment: Alignment.center,
-          margin: const EdgeInsets.all(20),
-          decoration: const BoxDecoration(
-            color: Colors.white,
+      appBar: AppBar(title: const Text("CSV Data")),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          60.h,
+          SizedBox(
+            height: 500,
+            child: Center(
+              child: Container(
+                width: double.infinity,
+                height: 500,
+                alignment: Alignment.center,
+                margin: const EdgeInsets.all(20),
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                ),
+                child: _csvData.isEmpty
+                    ? const Text("Selected Video", textAlign: TextAlign.center)
+                    : ListView.builder(
+                        itemCount: _csvData.length,
+                        itemBuilder: (context, index) {
+                          return ListTile(
+                            title: Text('Frame: ${_csvData[index][0]}'),
+                            subtitle: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text('Keypoints: ${_csvData[index][1]}'),
+                                Text('Elbow Angle: ${_csvData[index][2]}'),
+                                Text('Knee Angle: ${_csvData[index][3]}'),
+                              ],
+                            ),
+                          );
+                        },
+                      ),
+              ),
+            ),
           ),
-          child: _image == null
-              ? const Text(
-                  "Selected Video",
-                  textAlign: TextAlign.center,
-                )
-              : Image.file(_image!),
-        ),
+          _loadGalleryOrCameraButton(),
+          if (_isLoading)
+            const CircularProgressIndicator(), // Show loading indicator
+        ],
       ),
     );
   }
 
-  /// Open Gallery Or Camera Button Widget.
   Widget _loadGalleryOrCameraButton() {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         CustomButton(
           function: () async {
-            final pickedImage =
-                await picker.pickImage(source: ImageSource.gallery);
-            log("gallery");
+            log('open gallery');
+            final XFile? video =
+                await _picker.pickVideo(source: ImageSource.gallery);
             setState(() {
-              if (pickedImage != null) {
-                _image = File(pickedImage.path);
+              if (video != null) {
+                _video = video;
               }
             });
           },
@@ -98,12 +91,8 @@ class _CameraViewState extends State<CameraView> {
         CustomButton(
           function: () {
             log('UpLoad');
-            if (_image != null) {
-              Navigator.push(
-                context,
-                CupertinoModalBottomSheetRoute(
-                    builder: (e) => const ResultViewer(), expanded: false),
-              );
+            if (_video != null) {
+              requestVideoToFlask(_video!);
             } else {
               ScaffoldMessenger.of(context).showSnackBar(
                 const SnackBar(
@@ -116,5 +105,72 @@ class _CameraViewState extends State<CameraView> {
         )
       ],
     );
+  }
+
+  Future<void> requestVideoToFlask(XFile videoPath) async {
+    // Flask_url을 dotenv에 작성해서 사용함
+    String serverUrl = dotenv.get("FLASK_URL");
+
+    setState(() {
+      _isLoading = true; // Start loading
+    });
+
+    try {
+      var request = http.MultipartRequest('POST', Uri.parse(serverUrl));
+      request.files
+          .add(await http.MultipartFile.fromPath('video', videoPath.path));
+
+      var response = await request.send();
+
+      if (response.statusCode == 200) {
+        // Save CSV file
+        final bytes = await response.stream.toBytes();
+        final directory = await getApplicationDocumentsDirectory();
+        final csvFile = File('${directory.path}/output.csv');
+        await csvFile.writeAsBytes(bytes);
+
+        // Log CSV file path
+        log('CSV file saved at: ${csvFile.path}');
+
+        // Read CSV file content
+        List<List<dynamic>> csvContent = await _readCsvFile(csvFile);
+        setState(() {
+          _csvData = csvContent;
+        });
+      } else {
+        log('Server error: ${response.statusCode}');
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text("Error: ${response.statusCode}")),
+        );
+      }
+    } catch (e) {
+      log('Error sending video to Flask: $e');
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text("Failed to upload video")),
+      );
+    } finally {
+      setState(() {
+        _isLoading = false; // Stop loading
+      });
+    }
+  }
+
+  Future<List<List<dynamic>>> _readCsvFile(File file) async {
+    final contents = await file.readAsString();
+    List<List<dynamic>> rows = const CsvToListConverter().convert(contents);
+
+    // keypoints 문자열을 읽어와서 변환
+    for (var i = 1; i < rows.length; i++) {
+      // 첫 번째 줄은 헤더이므로 1부터 시작
+      String keypointsStr = rows[i][1].toString();
+      keypointsStr = keypointsStr
+          .replaceAll('np.float32(', '')
+          .replaceAll(')', '')
+          .replaceAll(' ', ''); // 불필요한 공백 제거
+
+      rows[i][1] = keypointsStr; // 변환된 keypoints 문자열을 넣습니다.
+    }
+
+    return rows;
   }
 }
