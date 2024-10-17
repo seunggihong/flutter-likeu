4a505e20f433ab9960e0771c3fc57e600d407679
snilsnil <heo6601@naver.com>
2024-10-17 23:42:48 +0900

    test by snilsnil

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_likeu/utils/app_colors.dart';
-
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_likeu/views/main/main_view.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 void main() async {
   await Hive.initFlutter();
   await Hive.openBox('users');
+  WidgetsFlutterBinding.ensureInitialized(); // 1번코드
+  await dotenv.load(fileName: ".env");
 
   runApp(const MyApp());
 }
