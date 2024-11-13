bfeec3e026ee65c95b3b118b50c8c0d374bc60ec
seunggi <hsk1533@naver.com>
2024-11-13 18:59:25 +0900

    fix: chart width size error [sumsung fold]

--- lib/views/chart/chart_view.dart
+++ lib/views/chart/chart_view.dart
@@ -24,7 +24,7 @@ class _ChartViewState extends State<ChartView> {
         showList.add(
           Container(
             margin: EdgeInsets.all(10),
-            width: MediaQuery.of(context).size.width - 30,
+            width: double.infinity,
             decoration: BoxDecoration(
               color: Colors.white,
               borderRadius: BorderRadius.circular(10),
